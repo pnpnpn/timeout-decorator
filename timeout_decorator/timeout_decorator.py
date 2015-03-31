560b2eb713b8fc31edb0ab45bd51f3a6deb14f8b
Aaron Elliot Ross <aaron@wawd.com>
2015-03-31 13:50:54 +0000

    Timeout can be fractional seconds.

--- timeout_decorator/timeout_decorator.py
+++ timeout_decorator/timeout_decorator.py
@@ -38,10 +38,11 @@ class TimeoutError(AssertionError):
 def timeout(seconds=None, use_signals=True):
     """Add a timeout parameter to a function and return it.
 
-    :param seconds: optional time limit in seconds. If None is passed, no timeout is applied.
+    :param seconds: optional time limit in seconds or fractions of a second. If None is passed, no timeout is applied.
         This adds some flexibility to the usage: you can disable timing out depending on the settings.
     :type seconds: float
     :param use_signals: flag indicating whether signals should be used for timing function out or the multiprocessing
+        When using multiprocessing, timeout granularity is limited to 10ths of a second.
     :type use_signals: bool
 
     :raises: TimeoutError if time limit is reached
