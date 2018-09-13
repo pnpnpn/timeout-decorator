8101c74061759e415d8d42a0cdf2a5c07e74e2af
Robert Nowotny <bitranox@users.noreply.github.com>
2018-09-13 18:17:59 +0200

    Update timeout_decorator.py

--- timeout_decorator/timeout_decorator.py
+++ timeout_decorator/timeout_decorator.py
@@ -1,15 +1,16 @@
 """
 Timeout decorator.
-    :copyright: (c) 2012-2013 by PN.
+    :copyright: (c) 2017 by Robert Nowotny
     :license: MIT, see LICENSE for more details.
 """
 
 
-import multiprocessing
-import multiprocessing.pool
+import dill          # dill is much more tolerant then pickle, so lets use
+import multiprocess  # multiprocess instead of multiprocessing
 import platform
 import signal
 import sys
+import threading
 import time
 import wrapt
 
@@ -19,7 +20,7 @@ import wrapt
 
 # http://www.saltycrane.com/blog/2010/04/using-python-timeout-decorator-uploading-s3/
 # Used work of Stephen "Zero" Chappell <Noctis.Skytower@gmail.com>
-
+# Used work of https://github.com/pnpnpn/timeout-decorator
 
 if sys.version_info < (3, 3):
     TimeoutError = AssertionError  # there is no TimeoutError below Python 3.3
@@ -37,49 +38,34 @@ def _raise_exception(exception, exception_message):
 
 def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exception_message=None, dec_allow_eval=False):
     """Add a timeout parameter to a function and return it.
-
     ToDo : Traceback information when using no_signals
            integrate tblib in order to get exceptions when use_signals=False
            (see https://pypi.python.org/pypi/tblib)
-
     Usage:
-
     @timeout(3)
     def foo():
         pass
-
     Overriding the timeout:
-
     foo(dec_timeout=5)
-
     Usage without decorating a function :
-
     def foo2(a,b,c):
         pass
-
     timeout(3)(foo2)(1,2,c=3)
-
     Usage with eval (beware, security hazard, no user input values here):
         read : https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html before usage !
-
     def class Foo(object):
         def __init__(self,x):
             self.x=x
-
         @timeout('instance.x', dec_allow_eval=True)
         def foo2(self):
             print('swallow')
-
         @timeout(1)
         def foo3(self):
             print('parrot')
-
     # or override via kwarg :
     my_foo = Foo(3)
     my_foo.foo2(dec_timeout='instance.x * 2.5 +1')
     my_foo.foo3(dec_timeout='instance.x * 2.5 +1', dec_allow_eval=True)
-
-
     :param dec_timeout: *       optional time limit in seconds or fractions of a second. If None is passed,
                                 no seconds is applied. This adds some flexibility to the usage: you can disable timing
                                 out depending on the settings. dec_timeout will always be overridden by a
@@ -92,33 +78,29 @@ def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exceptio
                                 be a security issue. This is very powerful, but is also very dangerous if you
                                 accept strings to evaluate from untrusted input.
                                 read: https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
-
                                 If enabled, the parameter of the function dec_timeout, or the parameter passed
                                 by kwarg dec_timeout will be evaluated if its type is string. You can access :
                                 wrapped (the function object)
                                 instance    Example: 'instance.x' - see example above or doku
                                 args        Example: 'args[0]' - the timeout is the first argument in args
                                 kwargs      Example: 'kwargs["max_time"] * 2'
-
     * all parameters starting with dec_ can be overridden via kwargs passed to the wrapped function.
-
     :type dec_timeout:          float
     :type use_signals:          bool
     :type timeout_exception:    Exception
     :type exception_message:    str
-
-
     :raises:                    TimeoutError if time limit is reached
-
     :returns:                   the Result of the wrapped function
-
     It is illegal to pass anything other than a function as the first parameter.
     The function is wrapped and returned to the caller.
-
     """
 
     @wrapt.decorator
     def wrapper(wrapped, instance, args, kwargs):
+        if (not b_signals) and (not dill.pickles(wrapped)):
+            s_err = 'can not pickle {wn}, bad types {bt}'.format(wn=wrapped.__name__, bt=dill.detect.badtypes(wrapped))
+            raise dill.PicklingError(s_err)
+
         exc_msg = exception_message                             # make mutable
         decm_allow_eval = kwargs.pop('dec_allow_eval', dec_allow_eval)  # make mutable and get possibly kwarg
         decm_timeout = kwargs.pop('dec_timeout', dec_timeout)   # make mutable and get possibly kwarg
@@ -143,14 +125,28 @@ def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exceptio
                 timeout_wrapper = _Timeout(wrapped, timeout_exception, exc_msg, decm_timeout)
                 return timeout_wrapper(*args, **kwargs)
 
-    # never use signals with windows - it wont work anyway
-    b_signals = False
-    if use_signals and not platform.system().lower().startswith('win'):
-        b_signals = True
+    # automatically disable signals when they cant be used
+    if can_use_timeout_signals():
+        b_signals = use_signals
+    else:
+        b_signals = False
+
     return wrapper
 
 
-def _target(queue, function, *args, **kwargs):
+def can_use_timeout_signals():
+    """ gives True when we can use timeout signals, otherwise False"""
+    if platform.system().lower().startswith('win'):  # on Windows we cant use Signals
+        return False
+    if sys.version_info < (3, 4):
+        # on old python use this method - we can only use Signals in the Main Thread
+        return isinstance(threading.current_thread(), threading._MainThread)
+    else:
+        # much nicer after python 3.4 - we can only use Signals in the Main Thread
+        return threading.current_thread() == threading.main_thread()
+
+
+def _target(child_conn, function, *args, **kwargs):
     """Run a function with arguments and return output via a queue.
     This is a helper function for the Process created in _Timeout. It runs
     the function with positional arguments and keyword arguments and then
@@ -158,9 +154,11 @@ def _target(queue, function, *args, **kwargs):
     raised, it is returned to _Timeout to be raised by the value property.
     """
     try:
-        queue.put((True, function(*args, **kwargs)))
+        child_conn.send((True, function(*args, **kwargs)))
     except:
-        queue.put((False, sys.exc_info()[1]))
+        child_conn.send((False, sys.exc_info()[1]))
+    finally:
+        child_conn.close()
 
 
 class _Timeout(object):
@@ -179,9 +177,9 @@ class _Timeout(object):
         self.__exception_message = exception_message
         self.__name__ = function.__name__
         self.__doc__ = function.__doc__
-        self.__timeout = time.time()
-        self.__process = multiprocessing.Process()
-        self.__queue = multiprocessing.Queue()
+        self.__process = None
+        self.__parent_conn = None
+        self.__child_conn = None
 
     def __call__(self, *args, **kwargs):
         """Execute the embedded function object asynchronously.
@@ -189,39 +187,33 @@ class _Timeout(object):
         requires that "ready" be intermittently polled. If and when it is
         True, the "value" property may then be checked for returned data.
         """
-        self.__queue = multiprocessing.Queue(1)
-        args = (self.__queue, self.__function) + args
-        self.__process = multiprocessing.Process(target=_target,
-                                                 args=args,
-                                                 kwargs=kwargs)
+        self.__parent_conn, self.__child_conn = multiprocess.Pipe(duplex=False)
+
+        args = (self.__child_conn, self.__function) + args
+        self.__process = multiprocess.Process(target=_target, args=args, kwargs=kwargs)
         self.__process.daemon = True
         self.__process.start()
-        self.__timeout = self.__limit + time.time()
-        while not self.ready:
-            time.sleep(0.01)
-        return self.value
+        if self.__parent_conn.poll(self.__limit):
+            return self.value
+        else:
+            self.cancel()
 
     def cancel(self):
         """Terminate any possible execution of the embedded function."""
+        self.__parent_conn.close()
         if self.__process.is_alive():
             self.__process.terminate()
+
         _raise_exception(self.__timeout_exception, self.__exception_message)
 
     @property
-    def ready(self):
-        """Read-only property indicating status of "value" property."""
-        if self.__timeout < time.time():
-            self.cancel()
-        return self.__queue.full() and not self.__queue.empty()
-
-    @property
     def value(self):
-        """Read-only property containing data returned from function."""
-        if self.ready is True:
-            flag, load = self.__queue.get()
-            # when self.__queue.get() exits, maybe __process is still alive,
-            # then it might zombie the process. so lets join it explicitly
-            self.__process.join(1)
-            if flag:
-                return load
-            raise load
+        flag, load = self.__parent_conn.recv()
+        self.__parent_conn.close()
+        # when self.__queue.get() exits, maybe __process is still alive,
+        # then it might zombie the process. so join it explicitly
+        self.__process.join(1)
+
+        if flag:
+            return load
+        raise load
