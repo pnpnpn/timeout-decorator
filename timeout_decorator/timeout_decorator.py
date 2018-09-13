9908a000d2efe00d6a7e5047003e49e9c18c976e
Robert Nowotny <bitranox@users.noreply.github.com>
2018-09-13 12:31:21 +0200

    Update timeout_decorator.py
    fix pep8

--- timeout_decorator/timeout_decorator.py
+++ timeout_decorator/timeout_decorator.py
@@ -219,8 +219,9 @@ class _Timeout(object):
         """Read-only property containing data returned from function."""
         if self.ready is True:
             flag, load = self.__queue.get()
-            self.__process.join(1)  # when self.__queue.get() exits, maybe __process is still alive,
-                                    # then it might zombie the process. so lets join it explicitly
+            # when self.__queue.get() exits, maybe __process is still alive,
+            # then it might zombie the process. so lets join it explicitly
+            self.__process.join(1)
             if flag:
                 return load
             raise load
