095ddc869e4336d2fe1c1c54060eb9a91471c521
Johann Christensen <johann.christensen@zal.aero>
2023-02-06 15:39:47 +0100

    Update timeoutd: new name, type hints

--- tests/test_timeoutd.py
+++ tests/test_timeoutd.py
@@ -3,7 +3,10 @@ import time
 
 import pytest
 
-from timeout_decorator import timeout, TimeoutError
+from timeoutd import timeout
+
+TIMEOUT = 0.1
+EXCEPTION_MESSAGE = "Timeout exceeded."
 
 
 @pytest.fixture(params=[False, True])
@@ -13,108 +16,117 @@ def use_signals(request):
 
 
 def test_timeout_decorator_arg(use_signals):
-    @timeout(1, use_signals=use_signals)
+    @timeout(seconds=TIMEOUT, use_signals=use_signals)
     def f():
         time.sleep(2)
+
     with pytest.raises(TimeoutError):
         f()
 
 
 def test_timeout_class_method(use_signals):
-    class c():
-        @timeout(1, use_signals=use_signals)
+    class c:
+        @timeout(seconds=TIMEOUT, use_signals=use_signals)
         def f(self):
             time.sleep(2)
+
     with pytest.raises(TimeoutError):
         c().f()
 
 
 def test_timeout_kwargs(use_signals):
-    @timeout(3, use_signals=use_signals)
+    @timeout(seconds=0.3, use_signals=use_signals)
     def f():
-        time.sleep(2)
+        time.sleep(0.2)
+
     with pytest.raises(TimeoutError):
-        f(timeout=1)
+        f(timeout=0.1)
 
 
 def test_timeout_alternate_exception(use_signals):
-    @timeout(3, use_signals=use_signals, timeout_exception=StopIteration)
+    @timeout(seconds=0.3, use_signals=use_signals, exception_type=StopIteration)
     def f():
-        time.sleep(2)
+        time.sleep(0.2)
+
     with pytest.raises(StopIteration):
-        f(timeout=1)
+        f(timeout=0.1)
 
 
 def test_timeout_kwargs_with_initial_timeout_none(use_signals):
     @timeout(use_signals=use_signals)
     def f():
-        time.sleep(2)
+        time.sleep(0.2)
+
     with pytest.raises(TimeoutError):
-        f(timeout=1)
+        f(timeout=0.1)
 
 
 def test_timeout_no_seconds(use_signals):
     @timeout(use_signals=use_signals)
     def f():
         time.sleep(0.1)
+
     f()
 
 
 def test_timeout_partial_seconds(use_signals):
-    @timeout(0.2, use_signals=use_signals)
+    @timeout(seconds=0.2, use_signals=use_signals)
     def f():
         time.sleep(0.5)
+
     with pytest.raises(TimeoutError):
         f()
 
 
 def test_timeout_ok(use_signals):
-    @timeout(seconds=2, use_signals=use_signals)
+    @timeout(seconds=0.2, use_signals=use_signals)
     def f():
-        time.sleep(1)
+        time.sleep(0.1)
+
     f()
 
 
 def test_function_name(use_signals):
-    @timeout(seconds=2, use_signals=use_signals)
+    @timeout(seconds=0.2, use_signals=use_signals)
     def func_name():
         pass
 
-    assert func_name.__name__ == 'func_name'
+    assert func_name.__name__ == "func_name"
 
 
 def test_timeout_pickle_error():
     """Test that when a pickle error occurs a timeout error is raised."""
-    @timeout(seconds=1, use_signals=False)
+
+    @timeout(seconds=TIMEOUT, use_signals=False)
     def f():
         time.sleep(0.1)
 
-        class Test(object):
+        class Test:
             pass
+
         return Test()
+
     with pytest.raises(TimeoutError):
         f()
 
 
 def test_timeout_custom_exception_message():
-    @timeout(seconds=1, exception_message="Custom fail message")
+    @timeout(seconds=TIMEOUT, exception_message=EXCEPTION_MESSAGE)
     def f():
         time.sleep(2)
-    with pytest.raises(TimeoutError, match="Custom fail message"):
+
+    with pytest.raises(TimeoutError, match=EXCEPTION_MESSAGE):
         f()
 
 
 def test_timeout_custom_exception_with_message():
-    @timeout(seconds=1, timeout_exception=RuntimeError, exception_message="Custom fail message")
+    @timeout(
+        seconds=TIMEOUT,
+        exception_type=RuntimeError,
+        exception_message=EXCEPTION_MESSAGE,
+    )
     def f():
         time.sleep(2)
-    with pytest.raises(RuntimeError, match="Custom fail message"):
-        f()
 
-
-def test_timeout_default_exception_message():
-    @timeout(seconds=1)
-    def f():
-        time.sleep(2)
-    with pytest.raises(TimeoutError, match="Timed Out"):
+    with pytest.raises(RuntimeError, match=EXCEPTION_MESSAGE):
         f()
---
+++ timeoutd/__init__.py
@@ -0,0 +1,4 @@
+"""timeoutd - A decorator to kill long running processes."""
+from timeoutd.timeoutd import timeout
+
+__all__ = ["timeout"]
